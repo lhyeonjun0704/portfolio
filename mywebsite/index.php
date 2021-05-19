61b5eca9f61e6ca80a6859cc2ac3753b6bc574be
Hyonjun Lee <hello.git.toqur7000@naver.com>
2021-05-19 15:07:42 +0900

    dir_re

--- mywebsite/index.php
+++ mywebsite/index.php
@@ -122,4 +122,50 @@
 
 					</div>
 
-<?php require('./php/footer.php') ?>
+<!-- Footer -->
+<footer id="footer">
+						<section>
+							<h2>Developer Lee</h2>
+							<p>Welcome! Here is Developer Lee's profile page. <br />
+								This page has been upadting  :)<br />
+								Thank you for your visit!<br />
+								If you want to contact me please send an email!
+							</p>
+							<ul class="actions">
+								<li><a href="https://developerlee.herokuapp.com/" class="button">Home</a></li>
+							</ul>
+						</section>
+						<section>
+							<h2>Personal Data</h2>
+							<dl class="alt">
+								<dt>Address</dt>
+								<dd>Secret</dd>
+								<dt>Phone</dt>
+								<dd>(010) 6325-058x</dd>
+								<dt>Email</dt>
+								<dd><a href="toqur7000@naver.com">toqur7000@naver.com</a></dd>
+							</dl>
+							<ul class="icons">
+								<li><a href="https://twitter.com/p47EgkFHHFBgk57" class="icon brands fa-twitter alt"><span class="label">Twitter</span></a></li>
+								<li><a href="https://www.facebook.com/profile.php?id=100003627801543" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.instagram.com/saebyeog.i/" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
+								<li><a href="https://github.com/lhyeonjun0704" class="icon brands fa-github alt"><span class="label">GitHub</span></a></li>
+								<li><a href="https://blog.naver.com/toqur7000" class="fas fa-blog"><span class="label">Blog</span></a></li>
+							</ul>
+						</section>
+						<p class="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			<script src="assets/js/download.js"></script>
+	</body>
+</html>
