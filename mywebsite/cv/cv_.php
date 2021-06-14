07d111589909b25cd27145f7a9b70db2d21cf6d7
Hyonjun Lee <hello.git.toqur7000@naver.com>
2021-06-14 14:45:02 +0900

    Update cv_.php

--- mywebsite/cv/cv_.php
+++ mywebsite/cv/cv_.php
@@ -34,9 +34,10 @@
 								<p>And then, I studied Python. I tried to code a program that predict lotto numbers by using Crawling. It made a great outcome the first week, but I found that the data is not sufficient for analyze and lotto show the characteristic of probability. So, I reckoned need of other skills or different approaches and I studied Machine Learning. Sikit-learn and Kaggle data were useful to try processing information. Then I wanted to make a project and I gathered team members. Although, having a narrow network in computer science field, I can gather team members in some open chatting of Kakaotalk. Our team participated a contest in Dacon. Our team marked high score and within 10 percent in the contest that more than 400 teams enrolled. I could experience that using Notion or Github for project.</p>
 								<p>I’m still updating this self-introduction.               Latest date -2021.05.14.</p>
 								<h2>다재다능한 인재상</h2>
-								<p>대학 생활을 중 교환학생 프로그램을 통해 미국의 오클라호마 대학에서 컴퓨터 프로그래밍 수업을 들으면서 어떤 다른 프로그래밍 언어를 쓰는지 배울 수 있었습니다. 특히 한 번도 접하지 못했던 Angular나 Microsoft Azure 같은 것들을 접해볼 수 있었던 좋은 기회가 되었습니다.
+								<p>대학 생활을 중 교환학생 프로그램을 통해 미국의 오클라호마 대학에서 컴퓨터 프로그래밍 수업을 들으면서 어떤 다른 프로그래밍 언어를 쓰는지 배울 수 있었습니다. 특히 한 번도 접하지 못했던 Angular나 Microsoft Azure 같은 것들을 접해볼 수 있었던 좋은 기회가 되었습니다.<br /><br />
 
 								귀국 이후는 Python을 주 언어로 선택하여 Django 나 크롤링이라는 기술을 스스로 익혀 페이지 소스를 끌어와 로또 숫자 예상 프로그램도 짜보았지만, 데이터의 양이 현저히 부족하고 확률 성이라는 특징을 강하게 띄고 있어 지속해서 맞추기 어렵다고 판단하고 다른 방향에서의 접근이나 기술이 필요하다고 느껴 기계학습 공부를 시작했습니다. Skit-Learn과 Gaggle의 타이태닉 데이터를 이용해 가공해보고 기술을 공부했고 그 이후 프로젝트를 경험해보기 위해 DACOM의 제주 신용 카드 빅데이터 예상 대회에 팀장의 역할을 맡아 직접 팀원을 모아 참여하였습니다. 컴퓨터공학이라는 분야에서 다소 저의 네트워크망이 좁았기 때문에 같이 프로젝트를 할 팀원을 모으기 쉽지 않았지만, 카카오톡의 수많은 공개채팅방을 통해서 직접 팀원을 모아 참가할 수 있었습니다. 회의를 모두 직접 모여 같이 진행할 수 있었지만, 코로나 문제로 인해 직접적인 만남을 지양했으며, 대체 방안으로 줌을 통해 수차례의 회의와 미팅을 할 수 있었습니다. 그리고 부가적으로 Notion 같은 현업에서 서로 소통하거나 기획하는 데에 큰 도움을 줄 수 있는 사이트를 알 있었고 Git hub을 팀 프로젝트에 처음으로 사용해 볼 수 있는 기회도 되었습니다.
+								<br /><br />
 
 								이러한 방식으로 프로젝트를 완료하여 400팀 이상이 참여하였던 대회에서 39등이라는 좋은 성과를 낼 수 있었습니다. 첫 팀 프로젝트라 원만하게 일 처리는 어려웠지만 다른 개발자들과 소통을 통해 많은 점을 배울 수 있었으며, 개발자들 간의 네트워크망이 필요하다고 느꼈습니다. 저의 혼자만의 개발이 아닌 협업을 통해 저 자신의 틀을 깰 좋은 기회가 되었다고 느꼈습니다. </p>
 							</section>
