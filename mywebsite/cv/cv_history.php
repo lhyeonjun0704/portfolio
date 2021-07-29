218dc2687d50696797bde7464af67c74d0e2fa47
lhyeonjun0704 <66072431+lhyeonjun0704@users.noreply.github.com>
2021-07-29 20:50:36 +0900

    0729

---
+++ mywebsite/cv/cv_history.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<!--
+	Stellar by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Developer Lee_History</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="../assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1>History_</h1>
+						<p>Still Updating...</p>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Content -->
+							<section id="content" class="main">
+								<span class="image main"><img src="../images/cv_img_re.jpg" alt="" /></span>
+								<h2>History</h2>
+								<p>This page still updating. Please Wait! </p>
+							</section>
+
+					</div>
+
+<?php require('../php/footer.php') ?>
